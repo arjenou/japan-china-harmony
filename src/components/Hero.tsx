f5a52a965e9039fc6d274fd9481ff5758a12c774
arjenou <wangyunjie1101@gmail.com>
2025-10-20 20:14:59 +0900

    update banner

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,6 +28,7 @@ const Hero = () => {
             className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${
               index === currentImageIndex ? "opacity-100" : "opacity-0"
             }`}
+            style={index === 1 ? { objectPosition: 'center 80%' } : { objectPosition: 'center' }}
             loading="eager"
           />
         ))}
